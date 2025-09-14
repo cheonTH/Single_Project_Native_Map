e622366bf8e0b0d8c68e60701e07557587b4423d
cheonTH <xogus0530@gmail.com>
2025-09-14 21:51:35 +0900

    .gitignore로 숨길 예정

--- src/api/AxiosApi.js
+++ src/api/AxiosApi.js
@@ -7,6 +7,6 @@ const hostname = window && window.location && window.location.hostname;
 
 backendHost = "http://springboot-developer-single.eba-49z7darg.ap-northeast-2.elasticbeanstalk.com"
 // backendHost = "http://localhost:5000"
-export const GOOGLE_API_KEY = "AIzaSyBEIKAZLpugydwBEN6Ku1703ih_z80hDIo";
+export const GOOGLE_API_KEY = "AIzaSyBEIKAZLpugydwBEN6Ku1703ih_z80hDIo";  
 
 export const API_BASE_URL = `${backendHost}`
